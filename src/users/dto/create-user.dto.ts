066bfa49366448db2acba2e1d4c54420c599f5bc
IvanZaborskih <ivan.31.03@mail.ru>
2022-11-07 14:21:48 +0300

    Add swagger docs for users

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,8 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class CreateUserDto {
+	@ApiProperty({ example: 'user@user.com', description: 'Email' })
 	readonly email: string;
+	@ApiProperty({ example: 'Qwerty123', description: 'Password' })
 	readonly password: string;
 }
